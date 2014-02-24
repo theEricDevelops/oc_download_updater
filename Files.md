8252beb540ecd34b566686e4182668d4976c9f84
Eric Baker <eric@trafficlight.me>
2014-02-24 00:23:41 -0500

    Created language file

---
+++ Files.md
@@ -0,0 +1,17 @@
+Files Involved & Their Roles
+============================
+
+## admin/controller/tool/download_updater.php
+
+
+
+***
+
+## admin/model/tool/download_updater.php
+
+
+***
+
+## admin/view/template/tool/download_updater.php
+
+The view file creates the button to enact the module, and that's pretty much it!
---
+++ admin/language/english/tool/download_updater.php
@@ -0,0 +1,18 @@
+<?php
+#####################################################################################
+#  Module Download Updater for Opencart 1.5.0 From Eric Baker ericbaker.me 			#
+#####################################################################################
+
+// Heading
+$_['heading_title']    = 'Download Updater';
+
+
+// Text
+$_['text_success']     = 'Success: Added downloads to %s products.';
+
+// Import
+$_['button_update']	   = 'Update Products';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify download updater!';
+?>
